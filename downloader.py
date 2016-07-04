ba7b7ef57131aa5b58f3199e049e37cae7b6aeb0
Vincent Tseng <zeroasan@gmail.com>
2016-07-05 00:39:29 +0800

    update downloader

--- downloader.py
+++ downloader.py
@@ -3,9 +3,12 @@ import subprocess
 
 def download(url, dist):
     print 'start downloading: ' + url
-    process = subprocess.Popen(['youtube-dl', "-o c:/downloads/video/%(uploader)s/%(title)s-%(id)s.%(ext)s", url], stdout=subprocess.PIPE)
-    process.wait()
-    print 'xxx'
+    try:
+        process = subprocess.check_output(['youtube-dl', "-o downloads/video/%(uploader)s/%(title)s-%(id)s.%(ext)s", url], stderr=subprocess.STDOUT,shell=True)
+    except subprocess.CalledProcessError as e:
+        print "exception: " + e.output
+    else:
+        print 'xxx'
 
 
-download("http://v.youku.com/v_show/id_XMTYyOTg3OTU2NA==?from=y1.3-dv-2016new-239-23143.225965.1-3", 'c:/video/')
+download("https://www.youtube.com/watch?v=HNOT_feL27Y", '')
